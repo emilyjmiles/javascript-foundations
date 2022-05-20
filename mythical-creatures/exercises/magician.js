a99c5800ffe4c1251a41c3b42c04f2e90efea5fc
Emily Miles <ms.emily.j.miles@gmail.com>
2022-05-20 17:11:46 -0400

    Add magician files

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,31 @@
+class Magician {
+  constructor(object) {
+    this.name = `The Great ${object.name}`;
+    this.assistant = object.assistant;
+    this.favoriteAccessory = object.clothing || 'top hat';
+    this.confidencePercentage = 10;
+  }
+
+  performIncantation(spellWord) {
+    return `${spellWord.toUpperCase()}!`;
+  }
+
+  performTrick() {
+    this.confidencePercentage += 10;
+    if (this.favoriteAccessory === 'top hat') {
+      return 'PULL RABBIT FROM TOP HAT';
+    } else {
+      return 'PULL DOVE FROM SLEEVE';
+    }
+  }
+
+  performShowStopper() {
+    if (this.confidencePercentage >= 100 && this.assistant === true) {
+      return 'WOW! The magician totally just sawed that person in half!';
+  } else {
+      return 'Oh no, this trick is not ready!';
+    }
+  }
+}
+
+module.exports = Magician;

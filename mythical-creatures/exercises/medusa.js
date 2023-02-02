8d24d53cef70872d9227b18aadb8015a96426e3b
Emily Miles <ms.emily.j.miles@gmail.com>
2023-02-01 22:58:32 -0700

    Add implementation code to pass all tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,27 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor (name) {
+    this.name = name;
+    this.statues = [];
+  };
+
+  gazeAtVictim = (victim) => {
+    let addStatue = new Statue(victim.name);
+    let freePerson;
+
+    if (this.statues.length === 3) {
+      let setFree = this.statues.shift();
+      freePerson = new Person(setFree.name);
+      freePerson.mood = `relieved`;
+    }
+    if (this.statues.length < 3) {
+      this.statues.push(addStatue);
+    }
+
+    return freePerson;
+  };
+};
+
+module.exports = Medusa;

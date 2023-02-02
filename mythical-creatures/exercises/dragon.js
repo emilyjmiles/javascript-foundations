df0d164401bc22ca376f58be869b485e51c7a540
Emily Miles <ms.emily.j.miles@gmail.com>
2023-02-01 17:34:42 -0700

    Add implementation code to pass all tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+  constructor (name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.mealCount = 0;
+  };
 
+  greet = () => {
+    return `Hi, ${this.rider}!`;
+  };
+
+  eat = () => {
+    this.mealCount++;
+
+    if (this.mealCount > 2) {
+      this.hungry = false;
+    }
+  };
+};
+
+module.exports = Dragon;

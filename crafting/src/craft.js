ff16b7f9f05ede2b1c17254443d76e10a83a840d
Emily Miles <ms.emily.j.miles@gmail.com>
2023-02-13 17:45:20 -0500

    Add implementation code to pass all tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,23 @@
 class Craft {
+  constructor (details) {
+    this.name = details.type;
+    this.materials = details.materials;
+    this.completed = false;
+  }
 
+  completeCraft = () => {
+    this.completed = true;
+
+    return `All done! It looks great!`;
+  };
+
+  calculateProjectTotal = () => {
+    return this.materials.reduce((total, material) => {
+      total += material.calculateMaterialCost();
+
+      return total;
+    }, 0);
+  };
 }
 
 module.exports = Craft;

b7a25e256b981723d76934e0247c1824fb2773e6
Emily Miles <ms.emily.j.miles@gmail.com>
2023-02-16 16:13:37 -0500

    Add implementation code to pass all tests

--- revisited-practice/do-you-wanna-build-a-snowman/src/Human.js
+++ revisited-practice/do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,32 @@
-var Snowman = require('./Snowman');
+var Snowman = require('./snowman');
 
 class Human {
+  constructor (name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    };
+  };
 
-}
+  gatherMaterials = (material, amount) => {
+    this.materials[material] += amount;
+  };
+
+  buildASnowman = () => {
+    return new Snowman(this.materials);
+  };
+
+  placeMagicHat = (snowman) => {
+    if (snowman.canWearMagicHat()) {
+      return `Woah, this snowman is coming to life!`;
+    }
+
+    return `I guess I didn't build it correctly.`;
+  };
+};
 
 module.exports = Human;
--- revisited-practice/do-you-wanna-build-a-snowman/test/Human-test.js
+++ revisited-practice/do-you-wanna-build-a-snowman/test/Human-test.js
@@ -1,21 +1,22 @@
 var assert = require("chai").assert;
-var Human = require("../src/Human");
-var Snowman = require("../src/Snowman");
+var Human = require("../src/human");
+var Snowman = require("../src/snowman");
 
 describe("Human", function () {
-  it.skip('should have a name', function () {
+
+  it('should have a name', function () {
     var human = new Human('Kayla');
 
     assert.equal(human.name, "Kayla");
   });
 
-  it.skip('should want to build a snowman', function () {
+  it('should want to build a snowman', function () {
     var human = new Human('Hannah');
 
     assert.equal(human.wantsToBuildASnowman, true);
   });
 
-  it.skip('should have no coal, snowballs, carrot or buttons to start', function () {
+  it('should have no coal, snowballs, carrot or buttons to start', function () {
     var human = new Human('Scott');
     var materials = {
       snowballs: 0,
@@ -27,7 +28,7 @@ describe("Human", function () {
     assert.deepEqual(human.materials, materials);
   });
 
-  it.skip('should be able to gather materials', function() {
+  it('should be able to gather materials', function () {
     var human = new Human('Pam');
 
     human.gatherMaterials('carrots', 1);
@@ -37,7 +38,7 @@ describe("Human", function () {
     assert.deepEqual(human.materials.buttons, 5);
   });
 
-  it.skip('should be able to add to existing materials', function () {
+  it('should be able to add to existing materials', function () {
     var human = new Human('Jeff');
 
     human.gatherMaterials('coal', 1);
@@ -48,7 +49,7 @@ describe("Human", function () {
     assert.deepEqual(human.materials.carrots, 1);
   });
 
-  it.skip('should be able to use gathered materials to build a snowman', function() {
+  it('should be able to use gathered materials to build a snowman', function () {
     var human = new Human('Leta');
 
     human.gatherMaterials('coal', 4);
@@ -63,7 +64,7 @@ describe("Human", function () {
     assert.equal(snowman.snowballs, 3);
   });
 
-  it.skip('should be able to put a magic hat on the snowman to make it come alive', function() {
+  it('should be able to put a magic hat on the snowman to make it come alive', function () {
     // HINT: You will need to call one of your snowman methods
     var human = new Human('scott');
 
@@ -77,10 +78,10 @@ describe("Human", function () {
     var hatPlacement = human.placeMagicHat(snowman);
 
     assert.equal(snowman.magicHat, true);
-    assert.equal(hatPlacement, 'Woah, this snowman is coming to life!')
+    assert.equal(hatPlacement, 'Woah, this snowman is coming to life!');
   });
 
-  it.skip('should be able to fail at putting a magic hat on the snowman', function () {
+  it('should be able to fail at putting a magic hat on the snowman', function () {
     var human = new Human('scott');
 
     human.gatherMaterials('coal', 0);
@@ -89,9 +90,9 @@ describe("Human", function () {
     human.gatherMaterials('snowballs', 5);
 
     var snowman = human.buildASnowman();
-    var hatPlacement = human.placeMagicHat(snowman)
+    var hatPlacement = human.placeMagicHat(snowman);
 
     assert.equal(snowman.magicHat, false);
-    assert.equal(hatPlacement, 'I guess I didn\'t build it correctly.')
+    assert.equal(hatPlacement, 'I guess I didn\'t build it correctly.');
   });
 });

788fa9fe83392a3fbbc62b77dcad8d1ea49ad3c9
Emily Miles <ms.emily.j.miles@gmail.com>
2023-02-16 19:55:56 -0500

    Add implementaion code to pass all tests

--- revisited-practice/lunchbox/lunchbox.js
+++ revisited-practice/lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
+  constructor (details) {
+    this.owner = details.owner;
+    this.material = details.madeOf;
+    this.shape = details.shape;
+    this.color = details.color;
+    this.snacks = [];
+  };
 
-}
+  acquireSnack = (snack) => {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  };
+
+  findHealthySnacks = () => {
+    return this.snacks.reduce((list, snack) => {
+      if (snack.checkForHealthy()) {
+        list.push(snack.type);
+      }
+
+      return list;
+    }, []);
+  };
+};
 
 module.exports = LunchBox;
--- revisited-practice/lunchbox/snack.js
+++ revisited-practice/lunchbox/snack.js
@@ -1,5 +1,27 @@
 class Snack {
+  constructor (snack) {
+    this.type = snack;
+    this.deliciousLevel = `extra`;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  };
 
-}
+  getEaten = () => {
+    this.amount -= 10;
+
+    if (this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  };
+
+  checkForHealthy = () => {
+    if (this.type.toLowerCase().includes(`fruit`)) {
+      return true;
+    }
+
+    return false;
+  };
+};
 
 module.exports = Snack;
